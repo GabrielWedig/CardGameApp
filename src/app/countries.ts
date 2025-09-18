66104ac5db198f4e8e0eb0e8fa43c80a83f5667d
Gabriel <gabrielwedig2@gmail.com>
2025-09-18 17:52:50 -0300

    WiP

---
+++ src/app/countries.ts
@@ -0,0 +1,63 @@
+export const COUNTRIES = [
+  {
+    id: 0,
+    name: 'Venezuela',
+    image: 'venezuel.png',
+  },
+  {
+    id: 1,
+    name: 'Colômbia',
+    image: 'colombia.png',
+  },
+  {
+    id: 2,
+    name: 'Chile',
+    image: 'chile.png',
+  },
+  {
+    id: 3,
+    name: 'Suriname',
+    image: 'suriname.png',
+  },
+  {
+    id: 4,
+    name: 'Paraguai',
+    image: 'paraguay.png',
+  },
+  {
+    id: 5,
+    name: 'Guiana',
+    image: 'guyana.png',
+  },
+  {
+    id: 6,
+    name: 'Peru',
+    image: 'peru.png',
+  },
+
+  {
+    id: 7,
+    name: 'Equador',
+    image: 'ecuador.png',
+  },
+  {
+    id: 8,
+    name: 'Argentina',
+    image: 'argentin.png',
+  },
+  {
+    id: 9,
+    name: 'Uruguai',
+    image: 'uruguay.png',
+  },
+  {
+    id: 10,
+    name: 'Brasil',
+    image: 'brazil.png',
+  },
+  {
+    id: 11,
+    name: 'Bolívia',
+    image: 'bolivia.png',
+  },
+];
